a4b70999aad43e321f1bbc9d72c8085fe36a66fd
gabriel lopes <tanakacvr@gmail.com>
2023-05-26 16:22:48 -0300

    finalizando o projeto

---
+++ src/scripts/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const questions = document.querySelectorAll("[data-faq-question]");
+
+  const menu = document.querySelector(".menu-burger-container");
+
+  const active = () => menu.classList.toggle("active");
+
+  menu.addEventListener("click", active);
+
+  for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener("click", abreOuFechaResposta);
+  }
+  function abreOuFechaResposta(elemento) {
+    const classe = "faq__questions__item--is-open";
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+  }
+});
